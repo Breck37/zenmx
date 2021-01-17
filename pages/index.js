ec20942b6b972a639013b3acb9069498855a7881
B-eck37 <brent.eckert7@gmail.com>
2021-01-16 20:50:36 -0700

    fix: init fastestLap logic, Table init

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 
 ///// EXAMPLES ////
@@ -22,6 +22,19 @@ export default function Home() {
   }, [raceResults]);
 
   console.log("RACE", raceResults);
+  const fastestLaps = useMemo(() => {
+    console.log("MEMO", raceResults);
+    if (!raceResults || !raceResults.length) return [];
+    return raceResults
+      .sort((a, b) => a.bestLap - b.bestLap)
+      .reduce((a, c) => {
+        console.log("A", a, c);
+        a.push({ rider: c.riderName, lap: c.bestLap });
+        return a;
+      }, []);
+  }, [raceResults]);
+
+  console.log(fastestLaps);
   return (
     <div className="container">
       <Head>
@@ -34,39 +47,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">ZenMx</a>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className="title">{fastestLaps[0]?.rider}</h1>
       </main>
 
       <footer>
