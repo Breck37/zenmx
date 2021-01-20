9b47c9df3b0146b0eaac9fa1215ad46499324214
B-eck37 <brent.eckert7@gmail.com>
2021-01-19 21:42:50 -0700

    fix: RiderRow improved props, structure

--- components/Results/RiderRow.jsx
+++ components/Results/RiderRow.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { RiderRowStyled } from "./styles";
 
-const RiderRow = ({ rider }) => {
+// Props //
+// rider {
+// position,
+// riderName,
+// team,
+// bestLap,
+// lastLap,
+// number,
+// currentLap,
+// bike,
+// }
+// row =  number
+
+const RiderRow = ({ rider, row, onClick }) => {
   const {
     position,
     riderName,
@@ -12,13 +25,18 @@ const RiderRow = ({ rider }) => {
     currentLap,
     bike,
   } = rider;
-  console.log({ rider, bike: bike.split(" ")[0].toLowerCase() });
   return (
-    <RiderRowStyled bike={bike.split(" ")[0].toLowerCase()}>
+    <RiderRowStyled
+      onClick={onClick}
+      row={row}
+      bike={bike.split(" ")[0].toLowerCase()}
+    >
       <div className="position">{position}</div>
       <div className="name">{riderName}</div>
       <div className="number">{number}</div>
-      <div className="team">{team}</div>
+      <div className="team">
+        <div>{team}</div>
+      </div>
       <div className="bestLap">{bestLap}</div>
       <div className="lastLap">{lastLap}</div>
       <div className="currentLap">{currentLap}</div>
--- components/Results/styles.js
+++ components/Results/styles.js
@@ -9,21 +9,62 @@ import styled from "styled-components";
 // background-color: ${({ bike }) => backgroundColors[bike]};
 
 export const TableStyled = styled.div`
+  position: relative;
   width: 75%;
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(
-    ${({ raceResultsLength }) => (raceResultsLength ? raceResultsLength : 0)},
+    ${({ raceResultsLength }) =>
+      raceResultsLength ? raceResultsLength + 1 : 0},
     48px
   );
+  border-radius: 8px;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.04), 0px 2px 8px rgba(0, 0, 0, 0.08);
+
+  > div:first-child {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+
+  > div:last-child {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
+
+  .overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export const RiderRowStyled = styled.div`
   border: solid 1px #000;
+  border-bottom: none;
   grid-column: 1 / 8;
   grid-row: ${({ row }) => row};
   display: flex;
   align-items: center;
+  height: 48px;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.04), 0px 2px 8px rgba(0, 0, 0, 0.08);
+
+  &:last-child {
+    border-bottom: 1px solid #000;
+  }
+
+  &:hover {
+    transform: scale(1.1);
+    z-index: 1000;
+  }
+
+  > div {
+    // border-right: 1px solid #000;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding-left: 8px;
+  }
 
   .position {
     grid-column: 1 / 1;
@@ -43,6 +84,15 @@ export const RiderRowStyled = styled.div`
   .team {
     grid-column: 4 / 1;
     flex: 1;
+    padding-right: 8px;
+    max-width: 242px;
+
+    > div {
+      overflow: hidden;
+      max-width: 215px;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
   }
 
   .bestLap {
