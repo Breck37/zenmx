0c2f9c5b1679b87eb2abcddc3338fc2a33bcaa26
B-eck37 <brent.eckert7@gmail.com>
2021-01-30 21:38:44 -0700

    chore: added images to public folder
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

---
+++ client/public/logos/index.js
@@ -0,0 +1,17 @@
+import honda from "./HondaLogo.jpeg";
+import kawasaki from "./KawiLogo.jpeg";
+import husqvarna from "./HuskieLogo.jpeg";
+import ktm from "./KTMLogo.jpeg";
+import yamaha from "./YamahaLogo.jpeg";
+import gasgas from "./GasGasLogo.jpeg";
+import suzuki from "./SuzukiLogo.jpeg";
+
+module.exports = {
+  honda,
+  kawasaki,
+  husqvarna,
+  ktm,
+  yamaha,
+  gasgas,
+  suzuki,
+};
