26d8c33d3e42d846437776df8b1cf69e2b6f8d18
B-eck37 <brent.eckert7@gmail.com>
2021-02-01 20:07:21 -0700

    chore: corrected mongo string, improved picks model
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- server/models/weeklyPicks.model.js
+++ server/models/weeklyPicks.model.js
@@ -3,26 +3,26 @@ const Schema = mongoose.Schema;
 
 const WeeklyPick = new Schema(
   {
-    week: { type: Number },
-    year: { type: String },
-    user: { type: String },
+    week: { type: Number, default: null },
+    year: { type: String, default: new Date().getFullYear() },
+    user: { type: String, default: null },
     smallBikePicks: [
       {
         _id: false,
-        riderName: String,
-        position: Number,
+        riderName: { type: String, default: null },
+        position: { type: Number, default: null },
         points: { type: Number, default: null },
       },
     ],
     bigBikePicks: [
       {
         _id: false,
-        riderName: String,
-        position: Number,
+        riderName: { type: String, default: null },
+        position: { type: Number, default: null },
         points: { type: Number, default: null },
       },
     ],
-    totalPoints: Number,
+    totalPoints: { type: Number, default: null },
   },
   { timestamps: true }
 );
