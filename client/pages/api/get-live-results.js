b09b7520aa6b60fa1d0a2255667e443a1ea9b139
B-eck37 <brent.eckert7@gmail.com>
2021-02-01 20:06:03 -0700

    fix: client side spi live results
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

---
+++ client/pages/api/get-live-results.js
@@ -0,0 +1,31 @@
+import crawler from "crawler-request";
+import {
+  mapper,
+  seasonMapper,
+  spliceResults,
+  spliceSeasonResults,
+  resultsMapper,
+  lapsMapper,
+} from "../../helpers";
+
+export default (req, res) => {
+  crawler("https://live.amasupercross.com/xml/sx/RaceResults.json")
+    .then((response) => {
+      if (response && !response.error) {
+        const formattedResponse = JSON.parse(response.text);
+        const raceResults = resultsMapper(formattedResponse.B);
+        const fastestLaps = lapsMapper(raceResults);
+        res.status(200).send({
+          raceResults,
+          fastestLaps,
+          session: formattedResponse.S,
+          round: formattedResponse.T,
+          fastLapLeader: {
+            riderNumber: formattedResponse.MBY,
+            lapTime: formattedResponse.MLT,
+          },
+        });
+      }
+    })
+    .catch((e) => console.error("/get-live-results", e));
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
   useEffect(() => {
     if (!raceResults || !raceResults.length) {
       axios
-        .get("http://localhost:3700/get-live-results")
+        .get("/api/get-live-results")
         .then(({ data }) => {
           console.log({ data });
           setResults(data.raceResults);
