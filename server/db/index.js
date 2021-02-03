54602a648495c63167a50c8d13207deafad15cff
B-eck37 <brent.eckert7@gmail.com>
2021-02-02 20:46:11 -0700

    chore: added alternate mongo connect string
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
+// local connection
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/modernmoto", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .catch((e) => {
+//     console.error("Connection error", e.message);
+//   });
+
+// qa connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/modernmoto", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://breck:7txucedDy2TDJHt@modernmotoqa.3elg2.mongodb.net/modernmotoqa?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .catch((e) => {
     console.error("Connection error", e.message);
   });
