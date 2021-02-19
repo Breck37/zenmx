d5fb3b1b95f70cc36e9cc9fed765b8d3b4d80707
B-eck37 <brent.eckert7@gmail.com>
2021-02-18 23:25:42 -0700

    fix: my mistake, forgotten file update
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/components/RiderSelect/RiderSelect.jsx
+++ client/components/RiderSelect/RiderSelect.jsx
@@ -4,7 +4,6 @@ import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
-import "../../styles/fonts.css";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
