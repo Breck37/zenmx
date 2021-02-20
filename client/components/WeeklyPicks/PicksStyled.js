88af9af5beb948364ffc56df5aa87828fd26e3e1
B-eck37 <brent.eckert7@gmail.com>
2021-02-20 12:43:13 -0700

    style: team/pick styles
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/components/WeeklyPicks/PicksStyled.js
+++ client/components/WeeklyPicks/PicksStyled.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export default styled.div``;
+export default styled.div`
+  width: 50%;
+  padding: 24px 32px;
+  background-color: rgba(9, 236, 233, 1);
+  border-radius: 4px;
+  margin-bottom: 24px;
+`;
--- client/styles/TeamStyled.js
+++ client/styles/TeamStyled.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export default styled.div`
-  margin: 0;
+  margin: 150px 0 0;
   width: 100vw;
   min-height: 100%;
   overflow: hidden;
   background-color: ${({ currentMode }) => (currentMode ? "#fff" : "#282828")};
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   color: ${({ currentMode }) => (currentMode ? "#282828" : "#fff")};
