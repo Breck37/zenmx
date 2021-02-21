43b1d9cf3628520e2ed611177c1d1e492ac6be85
B-eck37 <brent.eckert7@gmail.com>
2021-02-21 11:35:43 -0700

    fix: Button component
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ButtonStyled from "./ButtonStyled";
-
-const Button = ({ label }) => {
-  return <ButtonStyled>{label}</ButtonStyled>;
+import { Button } from "@material-ui/core";
+const ModernButton = ({ label, onClick, disabled }) => {
+  return (
+    <ButtonStyled onClick={onClick}>
+      <Button disabled={disabled}>{label}</Button>
+    </ButtonStyled>
+  );
 };
 
-export default Button;
+export default ModernButton;
--- client/components/Button/ButtonStyled.js
+++ client/components/Button/ButtonStyled.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
-import { MODERN_PURPLE } from "../../styles/colors";
+import { MODERN_PURPLE, MODERN_WHITE } from "../../styles/colors";
 
 export default styled.div`
-  padding: 16px;
   background-color: ${MODERN_PURPLE};
   border-radius: 4px;
+
+  .MuiButton-root {
+    color: ${MODERN_WHITE};
+    padding: 9px 16px;
+  }
 `;
