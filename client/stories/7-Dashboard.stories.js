bb772037a74cc363454bb73e0a9661bc70f0b402
B-eck37 <brent.eckert7@gmail.com>
2021-02-24 22:18:02 -0700

    chore: Dashboard story init
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

---
+++ client/stories/7-Dashboard.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Dashboard } from "../components";
+
+export default {
+  title: "Dashboard",
+  component: Dashboard,
+};
+
+export const DesktopDashboard = () => {
+  return <Dashboard />;
+};
