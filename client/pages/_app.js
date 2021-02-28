d5ef6e72e0378bff5537b4ae12ea9cb275f2774f
B-eck37 <brent.eckert7@gmail.com>
2021-02-28 11:07:46 -0700

    refactor: minor _app improvements
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 // import App from 'next/app'
 import React, { useState, useEffect, useMemo } from "react";
+import Head from "next/head";
 import { CurrentModeContext } from "../hooks";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { Header } from "../components";
@@ -13,9 +14,9 @@ function ModernMotoFantasy({ Component, pageProps }) {
   const router = useRouter();
   const { user } = pageProps;
 
-  const isLoginPage = useMemo(() => {
-    return router.pathname === "/login";
-  });
+  const isLoginOrLandingPage = useMemo(() => {
+    return Boolean(router.pathname === "/login" || router.pathname === "/");
+  }, [router.pathname]);
 
   useEffect(() => {
     const mode = localStorage.getItem("USER_CURRENT_MODE");
@@ -40,8 +41,12 @@ function ModernMotoFantasy({ Component, pageProps }) {
   return (
     <UserProvider user={user}>
       <AppStyled currentMode={currentMode}>
+        <Head>
+          <title>ModernMotoFantasy</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <CurrentModeContext.Provider value={currentMode}>
-          {!isLoginPage && (
+          {!isLoginOrLandingPage && (
             <Header
               tabs={defaultTabs}
               currentMode={currentMode}
