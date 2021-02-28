41c507efbc77a757a121e872423e8bc41904bced
B-eck37 <brent.eckert7@gmail.com>
2021-02-28 11:48:54 -0700

    fix: restore login import
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,7 +1,7 @@
 import { useUser } from "@auth0/nextjs-auth0";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
-import { useCurrentMode } from "../../hooks/darkMode";
+import { useCurrentMode } from "../../hooks/currentMode";
 import { LoginStyled } from "../../styles";
 
 const Login = () => {
