de7393293ad542064a999619c8628e2e9b884d73
B-eck37 <brent.eckert7@gmail.com>
2021-03-07 18:53:54 -0700

    fix: added : check for minute plus lap times
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- client/helpers/laps-mapper.js
+++ client/helpers/laps-mapper.js
@@ -3,16 +3,23 @@ module.exports = (raceResults) => {
 
   return raceResults
     .sort((a, b) => {
+      if (a.bestLap.includes(":") && b.bestLap.includes(":")) {
+        return (
+          parseFloat(a.bestLap.split(":")[1]) -
+          parseFloat(b.bestLap.split(":")[1])
+        );
+      }
+
       if (a === b) {
         return 0;
       }
       const aArr = a.bestLap.split("."),
         bArr = b.bestLap.split(".");
       for (let i = 0; i < Math.min(aArr.length, bArr.length); i++) {
-        if (parseInt(aArr[i]) < parseInt(bArr[i])) {
+        if (parseFloat(aArr[i]) < parseFloat(bArr[i])) {
           return -1;
         }
-        if (parseInt(aArr[i]) > parseInt(bArr[i])) {
+        if (parseFloat(aArr[i]) > parseFloat(bArr[i])) {
           return 1;
         }
       }
