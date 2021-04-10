f3c7cdd8ff631f30172d279072f3867f98105555
Breck37 <brent.eckert7@gmail.com>
2021-04-10 01:39:15 -0700

    fix: improved Button component
    Signed-off-by: Breck37 <brent.eckert7@gmail.com>

--- client/components/Button/ButtonStyled.js
+++ client/components/Button/ButtonStyled.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 import { MODERN_PURPLE, MODERN_WHITE } from "../../styles/colors";
 
 export default styled.div`
-  background-color: ${({ disabled }) =>
-    disabled ? "rgba(0, 0, 0, 0.2)" : MODERN_PURPLE};
+  background-color: ${({ disabled, currentMode }) =>
+    disabled && currentMode ? "rgba(0, 0, 0, 0.2)" : MODERN_PURPLE};
   border-radius: 4px;
   text-align: center;
+  width: 50%;
 
   .MuiButton-root {
     color: ${MODERN_WHITE};
     padding: 9px 16px;
   }
+
+  @media (max-width: 500px) {
+    width: ${({ small }) => (small ? "75%" : "100%")};
+  } ;
 `;
