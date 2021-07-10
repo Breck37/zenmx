d00cb0747a6a05f42066fbe56b21b442be0f3f73
Brent Eckert <brent.eckert7@gmail.com>
2021-07-10 06:31:30 -0700

    fix(LeagueCard): show class points
    Signed-off-by: Brent Eckert <brent.eckert7@gmail.com>

--- components/LeagueCard/LeagueCard.js
+++ components/LeagueCard/LeagueCard.js
@@ -2,13 +2,24 @@ import React from 'react';
 import LeagueCardStyled from './LeagueCardStyled';
 
 const LeagueCard = ({ leaguePicks }) => {
+  const renderPoints = (label, pickPoints) => {
+    if (!pickPoints) return null;
+
+    return (
+      <h4>
+        {label}: {pickPoints}
+      </h4>
+    );
+  };
   return (
     <LeagueCardStyled>
       {leaguePicks.map((pick) => {
         return (
           <div key={pick.user} className="pick">
             <h1>{pick.user}</h1>
-            <h4>{pick.totalPoints}</h4>
+            <h2>Total: {pick.totalPoints}</h2>
+            {renderPoints('450', pick.bigBikePoints)}
+            {renderPoints('250', pick.smallBikePoints)}
             {pick.bigBikePicks
               .sort((a, b) => a.position - b.position)
               .map((rider) => {
